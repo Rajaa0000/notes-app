a232f324d2aa06a533b5ec6d80acc345a251ed8a
Rajaa0000 <mathsrajaa@gmail.com>
2026-04-26 21:26:06 +0100

    changing the wesgi.py file

--- notesAppV1/wsgi.py
+++ notesAppV1/wsgi.py
@@ -3,8 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesAppV1.settings')
 
-# Auto-run migrations on startup
-from django.core.management import call_command
-call_command('migrate', '--run-syncdb')
-
 application = get_wsgi_application()
