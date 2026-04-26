1c3e1439878b9573db567ad4b7bf020034c68813
Rajaa0000 <mathsrajaa@gmail.com>
2026-04-26 20:00:53 +0100

    chaning wsgi.py file

--- notesAppV1/wsgi.py
+++ notesAppV1/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for notesAppV1 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notesAppV1.settings')
+
+# Auto-run migrations on startup
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb')
 
 application = get_wsgi_application()
